5578ab5e0ea9f1c5596ac4d5df0e17e24461d5a9
Thargelion <elwindwalker@gmail.com>
2018-06-26 20:44:53 -0300

    Ejemplo routes para pedir token loco

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,7 +29,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
+     *
      * @return void
      */
     public function report(Exception $exception)
@@ -40,12 +41,19 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $exception
+     *
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
+
+            return response(array("error" => "unauthorized"), 401);
+
+        }
+
         return parent::render($request, $exception);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,13 @@ Route::group(['middleware' => ['api', 'cors']], function () {
     Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::get('/unidad', 'UnidadController@index');
+    Route::get('/unidad', 'UnidadController@index')->middleware('auth:api');
     Route::get('/consorcio', 'ConsorcioController@index');
     Route::get('/factura', 'FacturaController@index');
     Route::post('auth/register', 'Auth\ApiRegisterController@register');
     Route::post('oauth/login', 'Auth\PassportController@login');
     Route::post('oauth/register', 'Auth\PassportController@register');
     Route::group(['middleware' => 'auth:api'], function () {
-        Route::post('get-details', 'API\PassportController@getDetails');
+        Route::post('get-details', 'Auth\PassportController@getDetails');
     });
 });
