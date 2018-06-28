1707d637087a9fc970e4492213375e7fc0f641b8
Thargelion <elwindwalker@gmail.com>
2018-06-28 15:27:46 -0300

    Model + Create + Table

---
+++ database/migrations/2018_06_28_180722_create_proveedors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProveedorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('localidad');
+            $table->string('provincia');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('gastos', function (Blueprint $table){
+            $table->dropForeign(['proveedor_id']);
+        });
+        Schema::dropIfExists('proveedors');
+    }
+}
