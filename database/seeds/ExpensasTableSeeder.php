13077251dd68884152cea09b399ce6af611fb220
Maximiliano De Pietro <elwindwalker@gmail.com>
2018-07-10 20:07:37 -0300

    fix array

--- database/seeds/ExpensasTableSeeder.php
+++ database/seeds/ExpensasTableSeeder.php
@@ -15,16 +15,16 @@ class ExpensasTableSeeder extends Seeder
         $faker = Factory::create();
 
         for ($i = 0; $i < 10; $i++) {
-		    Expensa::create([
-			    'id' => "Consorcio_$i",
-			    'unidad' => "Consorcio_$i",
-			    'año' => $faker->year,	
-			    'mes' => $faker->monthName,	
-			    'estado' => $faker->randomElement(['pago', 'impago']);
-			    'emision' => $faker->date,	
-			    'vencimiento' => $faker->date,	
-			    'importe' => $faker->randomFloat,				  
-		    ]);
-	    }
+            Expensa::create([
+                'id' => "Consorcio_$i",
+                'unidad' => "Consorcio_$i",
+                'año' => $faker->year,
+                'mes' => $faker->monthName,
+                'estado' => $faker->randomElement(['pago', 'impago']),
+                'emision' => $faker->date,
+                'vencimiento' => $faker->date,
+                'importe' => $faker->randomFloat,
+            ]);
+        }
     }
 }
