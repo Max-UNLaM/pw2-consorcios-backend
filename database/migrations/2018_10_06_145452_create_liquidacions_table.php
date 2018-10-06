106c179c132aeb20275574b3aa7ac004367d77ae
Daniel Marconi <daniel.marconi@despegar.com>
2018-10-06 11:57:55 -0300

    Se agrega modelo y migration de liquidacion

---
+++ database/migrations/2018_10_06_145452_create_liquidacions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLiquidacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('liquidacions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('mes');
+            $table->unsignedInteger('anio');
+            $table->unsignedInteger('consorcio_id');
+            $table->float('valor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('liquidacions');
+    }
+}
