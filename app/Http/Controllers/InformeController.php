d6e0e8fc9fe3b42a3cee49b297e1fa55e18729ad
Daniel Marconi <daniel.marconi@despegar.com>
2018-10-09 20:40:10 -0300

    Admin get all informes

--- app/Http/Controllers/InformeController.php
+++ app/Http/Controllers/InformeController.php
@@ -8,14 +8,36 @@ use Illuminate\Http\Request;
 class InformeController extends Controller
 {
     public function index(Request $request){
-        $size = $request->get('size') ? $request->get('size') : 5;
-
-        $informes = Informe::list()->paginate($size);
-
-        foreach ($informes->items() as $informe){
+        $id = $request->get('id');
+        if($id){
+            $informe = Informe::find($id);
             Informe::agregarInformacion($informe);
+            return $informe;
         }
 
-        return $informes;
+        $size = $request->get('size') ? $request->get('size') : 5;
+        $mes = $request->get('mes');
+        $anio = $request->get('anio');
+        $consorcioId = $request->get('consorcio_id');
+
+        if($mes && $anio && $consorcioId){
+            $informes = Informe::filterByMesAnioConsorcio($mes, $anio, $consorcioId)->get();
+            foreach ($informes as $item){
+                $informeId = $item->id;
+            }
+            $inf = Informe::find($informeId);
+            Informe::agregarInformacion($inf);
+            return $inf;
+        } else {
+            $informes = Informe::list()->paginate($size);
+
+            foreach ($informes->items() as $informe){
+                Informe::agregarInformacion($informe);
+            }
+
+            return $informes;
+        }
+
+
     }
 }
