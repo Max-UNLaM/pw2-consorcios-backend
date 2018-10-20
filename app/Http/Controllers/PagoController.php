81fc883e20c325e2ef4e8ae49ac9cc1b99541cea
Maximiliano De Pietro <elwindwalker@gmail.com>
2018-10-20 11:27:14 -0300

    Resolver conflictos

--- app/Http/Controllers/PagoController.php
+++ app/Http/Controllers/PagoController.php
@@ -20,14 +20,14 @@ class PagoController extends Controller
         $size = $request->get('size') ? $request->get('size') : 5;
         $user = User::find(Auth::user()->getAuthIdentifier());
         $id = $request->get('id');
-        if($id) return new PagoResource(Pago::find($id));
+        if ($id) return new PagoResource(Pago::find($id));
         $mes = $request->get('mes');
         $anio = $request->get('anio');
-        if($mes && $anio){
+        if ($mes && $anio) {
             $pagos = Pago::where('mes', $mes)->where('anio', $anio)->paginate($size);
             return new PagoCollection($pagos);
         }
-        if($user->isOperator()){
+        if ($user->isOperator()) {
             $pagos = DB::table('pagos')
                 ->join('facturas', 'facturas.id', '=', 'pagos.factura_id')
                 ->where('facturas.consorcio_id', $user->administra_consorcio)
@@ -43,7 +43,7 @@ class PagoController extends Controller
     public function user(Request $request)
     {
         $id = $request->get('id');
-        if($id) return new PagoResource(Pago::find($id));
+        if ($id) return new PagoResource(Pago::find($id));
         $size = $request->get('size') ? $request->get('size') : 5;
         $user = User::find(Auth::user()->getAuthIdentifier());
         if ($request->get('puerta')) return "PATOVA";
@@ -55,7 +55,8 @@ class PagoController extends Controller
         return new PagoCollection($pagos);
     }
 
-    public function paginate(Request $request){
+    public function paginate(Request $request)
+    {
         return Pago::paginate($request->get('size'));
     }
 
@@ -63,7 +64,7 @@ class PagoController extends Controller
     {
         return Pago::find($id);
     }
-    
+
     public function delete(Request $request)
     {
         $resp = Pago::destroy($request->get('id'));
@@ -82,11 +83,11 @@ class PagoController extends Controller
         $monto = $request->get('monto');
         $medioDePago = $request->get('medio_de_pago');
         $codigoComprobante = null;
-
-        if(!$facturaId) return response("El campo factura_id es obligatorio", 400);
-        if(!$monto) return response("El campo monto es obligatorio", 400);
-        if(!$medioDePago) return response("El campo medio_de_pago es obligatorio", 400);
-        if($user->isAdmin() == 0 && $user->isOperator() == 0){
+        if (!$facturaId) $errores['factura'] = 'El campo factura_id es obligatorio';
+        if (!$monto) $errores['monto'] = "El campo monto es obligatorio";
+        if (!$medioDePago) $errores['medioDePago'] = "El campo medio de pago es obligatorio";
+        if (!empty($errores)) return response()->json($errores, 400);
+        if ($user->isAdmin() == 0 && $user->isOperator() == 0) {
             $codigo = $request->get('codigo_comprobante');
             if(!$codigo) return response("El codigo codigo_comprobante es obligatorio para usuarios que no son adminsitradores ni operadores",400);
             if(strlen($codigo) < 4) return response("El codigo_comprobante debe tener un minimo de 4 caracteres", 400);
@@ -125,7 +126,7 @@ class PagoController extends Controller
             $pago->factura_id = $request->get('factura_id');
             $pago->fecha = $request->get('fecha');
             $pago->monto = $request->get('monto');
-           
+
             //Guardo los cambios
             $pago->save();
 
@@ -138,10 +139,11 @@ class PagoController extends Controller
         }
     }
 
-    public function filterByStatus(Request $request){
+    public function filterByStatus(Request $request)
+    {
         $status = $request->get('status');
-        if(!$status) return response("El parametro status es obligatorio", 400);
-        if($status != 'APROBADO' && $status != 'APROBACION_PENDIENTE' && $status != 'RECHAZADO') return response("Los posibles valores del campo status son: APROBADO, APROBACION_PENDIENTE o RECHAZADO", 400);
+        if (!$status) return response("El parametro status es obligatorio", 400);
+        if ($status != 'APROBADO' && $status != 'APROBACION_PENDIENTE' && $status != 'RECHAZADO') return response("Los posibles valores del campo status son: APROBADO, APROBACION_PENDIENTE o RECHAZADO", 400);
 
         $size = $request->get('size') ? $request->get('size') : 5;
         $pagos = Pago::where('estado', $status)->orderByDesc('fecha')->paginate($size);
@@ -149,24 +151,26 @@ class PagoController extends Controller
         return new PagoCollection($pagos);
     }
 
-    public function approve(Request $request){
+    public function approve(Request $request)
+    {
         $id = $request->get('id');
-        if(!$id) return response("El campo id es obligatorio", 400);
+        if (!$id) return response("El campo id es obligatorio", 400);
 
         $pago = Pago::find($id);
-        if($pago->estado != 'APROBACION_PENDIENTE') return response("La factura seleccionada no tiene el estado APROBACION_PENDIENTE", 400);
+        if ($pago->estado != 'APROBACION_PENDIENTE') return response("La factura seleccionada no tiene el estado APROBACION_PENDIENTE", 400);
         $pago->aprobarPago();
         $pago->update();
 
         return $pago;
     }
 
-    public function refuse(Request $request){
+    public function refuse(Request $request)
+    {
         $id = $request->get('id');
-        if(!$id) return response("El campo id es obligatorio", 400);
+        if (!$id) return response("El campo id es obligatorio", 400);
 
         $pago = Pago::find($id);
-        if($pago->estado != 'APROBACION_PENDIENTE') return response("La factura seleccionada no tiene el estado APROBACION_PENDIENTE", 400);
+        if ($pago->estado != 'APROBACION_PENDIENTE') return response("La factura seleccionada no tiene el estado APROBACION_PENDIENTE", 400);
         $pago->estado = 'RECHAZADO';
         $pago->update();
 
